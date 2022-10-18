638286af0ebebe2f9459892f66b0873e1793a55d
IdahC <idahjepkemboi642@gmail.com>
2022-10-18 11:21:27 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include<stdio.h>														/**								*main-starts	here						
-								*/							
-int main(void)							{																	puts("\"Programming is like building a multilingual 			puzzle");						return	(0);	
- }	
+#include<stdio.h>														/**
+																 *main-starts here					
+																*/							
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);	
+}
+
