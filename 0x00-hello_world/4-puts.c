ec2ee02fb8014b774c558ed51078219c12f36ed8
IdahC <idahjepkemboi642@gmail.com>
2022-10-18 11:42:26 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
  * main is important
  * void because no integer
  * return 0 is for succes
- *
- * */							
+*/							
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
