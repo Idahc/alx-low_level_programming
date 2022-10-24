735af8b93a0e1dd828a44361be523da9dfc24dbf
IdahC <idahjepkemboi642@gmail.com>
2022-10-24 12:30:00 -0700

    Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks upper case alphas
+ * @c: an integer parameter
+ *Return: something but success
+ */
+
+int _isupper(int c)
+{
+
+	return (c >= 'A' && c <= 'Z');
+
+
+}
